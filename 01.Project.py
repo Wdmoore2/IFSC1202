021f7ff29be58342fe60b52f2df1880ecd28d43d
Wdmoore2 <wdmoore2@ualr.edu>
2024-08-27 23:10:02 +0000

    Changes to the codes.

--- 01.Project.py
+++ 01.Project.py
@@ -1,10 +1,9 @@
 length = int (input ("Enter length of time in days"))
 years = (length) // 365 
 length = (length) - (years*365)
-weeks = int(length) // 7
-#days = int(length) % (weeks)
-print (years)
-print (weeks)
-#print(days)
-
-
+weeks = (length) // 7 
+length =(length) - (weeks*7)
+days = (length)
+print ("Years:" + str (years))
+print ("Weeks:" + str (weeks))
+print ("Days:" + str(days))
