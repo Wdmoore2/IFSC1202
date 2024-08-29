97b11bd03d3207f8d0b62645d755cf6bd17aff53
Wdmoore2 <wdmoore2@ualr.edu>
2024-08-29 21:39:39 +0000

    Changes to the code. Added new code

---
+++ 03.01 Minimum of Three Numbers.py
@@ -0,0 +1,16 @@
+x =int(input ("Enter First number:"))
+y=int(input ("Enter Second Number:"))
+z=int(input ("Enter Third Number:"))
+if x>0:
+    print (x)
+else:
+    print (-x)
+if y>0:
+    print (y)
+else:
+    print (-y)
+if z>0:
+    print (z)
+else:
+    print (-z)
+
