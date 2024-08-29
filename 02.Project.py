67234347b30981a89c3d459cf2fd6bae739834f6
Wdmoore2 <wdmoore2@ualr.edu>
2024-08-29 21:53:05 +0000

    First draft of Hebrons Formula code

---
+++ 02.Project.py
@@ -0,0 +1,7 @@
+a =(input ("Enter Side 1:"))
+b = (input ("Enter Side 2:") )
+c = (input ("Enter Side 3:") )
+s = float(a + b + c /2)
+import math
+answer = math.sqrt (s * (s-c)* (s-b)*(s-c))
+print (answer)
